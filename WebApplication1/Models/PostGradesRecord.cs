3424a536b6edf9047e93219adf48a28310a7b4de
裕安 蔡 <a0909219938@gmail.com>
2021-11-26 13:29:11 +0800

    Bug fixed

--- WebApplication1/Models/PostGradesRecord.cs
+++ WebApplication1/Models/PostGradesRecord.cs
@@ -7,5 +7,17 @@ namespace WebApplication1.Models
 {
     public class PostGradesRecord
     {
+        public int id { get; set; }//Unity端省略 預設為0
+
+        public string StudentCode { get; set; }
+
+        public int grades { get; set; }
+
+        public int Corrects { get; set; }
+
+        public int Incorrects { get; set; }
+        public string PlayTime { get; set; }
+
+        public List<QuestionRecord> Records { get; set; }
     }
 }
